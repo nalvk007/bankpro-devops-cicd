34e42078822539c89b08e68417a021d025a749bc
nalvk007 <nalvk@yahoo.com>
2026-06-12 18:35:51 -0700

    modify the status

--- app/src/main/java/com/bankpro/app/controller/AccountController.java
+++ app/src/main/java/com/bankpro/app/controller/AccountController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class AccountController {
-
-    @GetMapping("/status")
-    public String status() {
-        return "BankPro Service Running on AWS";
+    @GetMapping("/")
+    public String home() {
+        return "BankPro Service is Running";
     }
 }
